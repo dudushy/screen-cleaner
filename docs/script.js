084c3e2d6861bd5061618b60f4e8141ded9eaebd
dudushy <eduardotalarico1337@gmail.com>
2022-07-13 09:10:54 -0300

    Enter/leave fullscreen; show/hide elements; change backgroundg color

--- docs/script.js
+++ docs/script.js
@@ -1,59 +1,65 @@
 console.log("screen-cleaner");
 
-function clearElements() {
-    var elements = document.getElementsByTagName("element");
+const divBackground = document.querySelector("#background");
+
+document.addEventListener("fullscreenchange", function() {
+	if(document.fullscreenElement !== null) {
+        console.log("FULLSCREEN [ON]");
+        hideElements();
+    } else {
+        console.log("FULLSCREEN [OFF]");
+        showElements();
+
+        changeBackgroundColor(divBackground, "default");
+    }
+});
+
+function showElements() {
+    console.log("showElements");
+
+    var elements = document.getElementsByClassName("element");
+    // console.log("elements", elements);
+
     for (const element of elements) {
+        // console.log("element", element);
+        element.style.display = "block";
+    }
+}
+
+function hideElements() {
+    console.log("hideElements");
+
+    var elements = document.getElementsByClassName("element");
+    // console.log("elements", elements);
+
+    for (const element of elements) {
+        // console.log("element", element);
         element.style.display = "none";
     }
 }
 
-function requestFullScreen() {
-    var body = document.body;
-    var requestMethod = body.requestFullScreen || body.webkitRequestFullScreen || body.mozRequestFullScreen || body.msRequestFullScreen;
-
-    if (requestMethod) { 
-        requestMethod.call(body);
-    } else if (typeof window.ActiveXObject !== "undefined") {
-        var wscript = new ActiveXObject("WScript.Shell");
-
-        if (wscript !== null) {
-            wscript.SendKeys("{F11}");
-        }
+function changeBackgroundColor(element, color) {
+    if (color == "default") {
+        element.style.backgroundColor = "#000000";
+    } else {
+        element.style.backgroundColor = color;
     }
+}
+
+function elementFullScreen(element, color) {
+    element.requestFullscreen()
+        .then(function () {
+            console.log("requestFullscreen [SUCCESS]");
+
+            changeBackgroundColor(divBackground, color);
+        })
+        .catch(function (error) {
+            console.log("requestFullscreen [ERROR]", error.message);
+        });
 }
 
 function goFullScreen(color) {
     console.log("goFullScreen || color =", color);
 
-    switch (color) {
-        case "white":
-            clearElements();
-            document.body.style.backgroundColor = color;
-            requestFullScreen();
-            break;
-
-        case "black":
-            clearElements();
-            document.body.style.backgroundColor = color;
-            requestFullScreen();
-            break;
-
-        case "red":
-            clearElements();
-            document.body.style.backgroundColor = color;
-            requestFullScreen();
-            break;
-
-        case "green":
-            clearElements();
-            document.body.style.backgroundColor = color;
-            requestFullScreen();
-            break;
-        
-        case "blue":
-            clearElements();
-            document.body.style.backgroundColor = color;
-            requestFullScreen();
-            break;
-    }
+    elementFullScreen(divBackground, color);
 }
